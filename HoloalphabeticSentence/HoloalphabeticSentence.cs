849a6760815acd2d377d6de338fc2d4f68adf92f
Roiit2912 <rohit.rathi2912@gmail.com>
2018-09-10 16:11:24 +0530

    assignment2 added

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -5,10 +5,42 @@ namespace HoloalphabeticSentence
 {
     public class HoloalphabeticSentence
     {
-        // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
+        
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            return false;
+            
+            bool[] mark = new bool[26];
+ 
+        int index = 0;
+ 
+        // Traverse all characters
+        for (int i = 0; i < input.Length; i++)
+        {
+            // If uppercase character, subtract 'A' to find index.
+            if ('A' <= input[i] && 
+                    input[i] <= 'Z')
+                         
+                index = input[i] - 'A';
+ 
+                // If lowercase character, subtract 'a' to find  index.
+                
+            else if('a' <= input[i] && 
+                        input[i] <= 'z')
+                             
+                index = input[i] - 'a';
+ 
+            // Mark current character
+            mark[index] = true;
+        }
+ 
+        // Return false if any character is unmarked
+        for (int i = 0; i <= 25; i++)
+            if (mark[i] == false)
+                return (false);
+ 
+        // If all characters were present
+        return (true);
+
         }
     }
 }
